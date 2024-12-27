ad7b0e6921de696b3b52aa10870d9f6ee5993cf1
Бодров <nikita247086@gmail.com>
2024-12-27 09:44:42 +0500

    Содание третьего фрагмента HelpFragment

--- app/src/main/java/com/hfad/catchat/HelpFragment.kt
+++ app/src/main/java/com/hfad/catchat/HelpFragment.kt
@@ -4,10 +4,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-class InboxFragment : Fragment() {
+class HelpFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_inbox, container, false)
+        return inflater.inflate(R.layout.fragment_help, container, false)
     }
 }
